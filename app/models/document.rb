7d30965e5a62be4dcc48d79c630f441da76d8d22
Tim <tim@chromasplosion.com>
2011-05-05 08:57:26 -0500

    Removed Rails, this was going to be a node.js project

--- app/models/document.rb
+++
@@ -1,4 +0,0 @@
-class Document < ActiveRecord::Base
-  has_many :replies
-  has_many :user_documents
-end
--- app/models/user_documents.rb
+++
@@ -1,4 +0,0 @@
-class UserDocuments < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :document
-end
